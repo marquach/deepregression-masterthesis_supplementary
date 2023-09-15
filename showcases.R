b5bc12cd7eca5ba89ac23edb4ca432b369f0988f
Christopher Marquardt <ch.marquardt@campus.lmu.de>
2023-09-15 10:10:29 +0200

    update showcases.R

--- showcases.R
+++ showcases.R
@@ -37,7 +37,7 @@ mod_torch
 # default lr = 0.001 change to 0.1 
 #mod_torch$model <- mod_torch$model  %>% set_opt_hparams(lr = 0.01)
 # torch approach does have a generic fit function 
-mod_torch %>% fit(epochs = 200, early_stopping = F, validation_split = 0)
+mod_torch %>% fit(epochs = 500, early_stopping = F, validation_split = 0)
 
 res_toy <- t(round(cbind(
   'gamlss' = c(toy_gamlss$mu.coefficients, toy_gamlss$sigma.coefficients),
@@ -61,17 +61,20 @@ round(sapply(list(mod_torch_res, toy_gamlss_res), function(x)
 distr_torch <- get_distribution(mod_torch)
 distr_torch_res <- data.frame(mu = as.array(distr_torch$mean),
                               sigma = as.array(distr_torch$stddev),
-                              package = "deepregression")
+                              Package = "deepregression")
 distr_gamlss_res <- data.frame(mu = toy_gamlss$mu.fv,
                                sigma = toy_gamlss$sigma.fv,
-                               package = "gamlss")
+                               Package = "gamlss")
 distr_res <- cbind(x=x, rbind(distr_torch_res, distr_gamlss_res))
-ggplot(data = distr_res,  aes(x = x, y = mu, col = package))+
-  geom_line()+
+distr_res <- tidyr::pivot_longer(distr_res, cols = c(mu, sigma))
+plot_gamlss <- ggplot(data = distr_res,  aes(x = x, y = value, col = Package))+
+  geom_line(show.legend = T)+
+  facet_wrap( ~name, scales = "free")+
   theme_classic()
-ggplot(data = distr_res,  aes(x = x, y = sigma, col = package))+
-  geom_line()+
-  theme_classic()
+plot_gamlss
+
+path <- "/Users/marquach/Desktop/github/deepregression-masterthesis_supplementary/results/numerical-experiments/"
+ggsave(filename = paste0(path, "plot_gamlss.png"), width = 14, height = 8)
 
 ################################################################################
 ################################################################################
